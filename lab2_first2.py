cd5936c704f8663e2c411fd4ebc7041993d026cc
MinerOfSouls <58173484+MinerOfSouls@users.noreply.github.com>
2025-03-18 18:36:32 +0100

    fixed lu

--- lab2_first2.py
+++ lab2_first2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time as time
 
 def gauss_solve(equation: np.array, A: np.array):
     assert equation.shape[0] ==  equation.shape[1]
@@ -29,15 +30,49 @@ def lu_factor(A: np.array):
 
     N = A.shape[0]
     for i in range(N):
-        A[i,i:] = A[i, i:] / A[i,i]
         for j in range(i+1,N):
-            m = A[j, i]*A[i,i]
-            A[j, i:] = A[j, i:] - (A[i, i:]*A[j, i])
+            m = A[j, i]/A[i,i]
+            A[j, i:] = A[j, i:] - (A[i, i:]*m)
             A[j, i] = m
     return A
 
-print(lu_factor(np.array([
-    [1.0,2.0,3.0],
-    [4.0,5.0,7.0],
-    [7.0,8.0,9.0]
-])))
+def test_LU():
+    random_matrix = [
+    np.random.rand(6, 6),
+    np.random.rand(500, 500),
+    np.random.rand(750, 750),
+    np.random.rand(1000, 1000),
+    np.random.rand(2000, 2000)]
+
+    for A in random_matrix:
+        copy = A.copy()
+        LU = lu_factor(A)
+        L = np.zeros(shape=LU.shape)
+        U = np.zeros(shape=LU.shape)
+        for i in range(LU.shape[0]):
+            L[i,:i] = LU[i,:i]
+            L[i,i] = 1
+            U[i, i:] = LU[i, i:]
+        print(np.linalg.matrix_norm(copy - np.matmul(L, U)))
+
+def test_solve():
+    random_matrix = [
+    np.random.rand(6, 6),
+    np.random.rand(500, 500),
+    np.random.rand(750, 750),
+    np.random.rand(1000, 1000),
+    np.random.rand(2000, 2000)]
+    random_spaces = [
+        np.random.rand(500),
+        np.random.rand(500),
+        np.random.rand(750),
+        np.random.rand(1000),
+        np.random.rand(2000)]
+
+    for i in range(5):
+        start = time.process_time()
+        sol_A = gauss_solve(random_matrix[i], random_spaces[i])
+        end1 = time.process_time()
+        sol_B = np.linalg.solve(random_matrix[i], random_spaces[i])
+        end2 = time.process_time()
+        print(random_matrix[i].shape, end1-start, end2-end1)
